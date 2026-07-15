3bb1de0c58898bdf6ea88813c27cb86fdd62deb3
Riyanzakaria <zriyan191@gmail.com>
2026-07-15 20:28:16 +0700

    chore: remove migrate from debug-path

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Route::get('/debug-path', function() {
     \Illuminate\Support\Facades\Artisan::call('config:clear');
     \Illuminate\Support\Facades\Artisan::call('view:clear');
     \Illuminate\Support\Facades\Artisan::call('cache:clear');
-    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
     
     return response()->json([
         'base_path' => base_path(),
