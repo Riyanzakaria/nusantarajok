5544197d7c3bdedebc10c99a0c4e609bb8869d12
Riyanzakaria <zriyan191@gmail.com>
2026-07-15 21:23:26 +0700

    fix: make the grid item itself sticky with self-start to ensure CSS grid sticky works

--- resources/views/checkout/create.blade.php
+++ resources/views/checkout/create.blade.php
@@ -155,9 +155,9 @@
             </div>
 
             <!-- RIGHT COLUMN: Summary (Sticky) -->
-            <div class="md:col-span-5 lg:col-span-4 relative hidden md:block">
+            <div class="md:col-span-5 lg:col-span-4 hidden md:block sticky top-28 self-start h-fit z-10">
                 <!-- Desktop Sidebar -->
-                <div class="sticky top-28 h-fit flex flex-col p-8 rounded-2xl" style="background: {{ $surface }}; border: 1px solid {{ $border }};">
+                <div class="flex flex-col p-8 rounded-2xl" style="background: {{ $surface }}; border: 1px solid {{ $border }};">
                     <p class="font-sans text-xs font-700 uppercase tracking-wider mb-6" style="color: {{ $inkPri }};">Ringkasan Pesanan</p>
                     
                     <div class="flex gap-4 mb-6">
